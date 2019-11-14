4b675dd0c0b03b93d328c6df409df2c62f703db2
Sergio Girado <sergiogirado@hotmail.com>
2019-11-13 21:27:27 -0500

    port to custom element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { FormsModule } from '@angular/forms';
 
 import { GreetComponent } from './greet.component';
 
 @NgModule({
-  declarations: [
-    GreetComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [GreetComponent]
+  declarations: [GreetComponent],
+  imports: [BrowserModule, FormsModule],
+  entryComponents: [GreetComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) { }
+
+  ngDoBootstrap() {
+    const GreetElement = createCustomElement(GreetComponent, { injector: this.injector });
+    customElements.define('app-greeter', GreetElement);
+  }
+}
--- src/app/greet.component.ts
+++ src/app/greet.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-greeter',
   templateUrl: 'greet.component.html'
 })
 export class GreetComponent {
